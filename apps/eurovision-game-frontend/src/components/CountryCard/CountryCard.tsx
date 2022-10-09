3f34d63926832c3ee5c6b9dcc9da917748056deb
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2022-10-09 17:53:10 +0300

    FEAT | migrate UI from POC

---
+++ apps/eurovision-game-frontend/src/components/CountryCard/CountryCard.tsx
@@ -0,0 +1,40 @@
+import { Card, Grid, Typography } from "@mui/material";
+import VoteButton from "components/VoteButton/VoteButton";
+import { styles } from "./CountryCard.styles";
+
+interface ICountryCardProps {
+  country: string;
+  artist: string;
+  song: string;
+  toggleModal: () => void;
+}
+
+const CountryCard: React.FC<ICountryCardProps> = ({
+  country,
+  artist,
+  song,
+  toggleModal,
+}) => (
+  <Card variant="outlined" sx={styles.card}>
+    <Grid container direction="column">
+      <Grid item>
+        <Typography variant="overline">{country}</Typography>
+      </Grid>
+      <Grid container item>
+        <Grid item>
+          <Typography variant="subtitle1">{artist}</Typography>
+        </Grid>
+        <Grid item sx={styles.gap}>
+          <Typography variant="subtitle1" sx={styles.song}>
+            {song}
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid item sx={styles.button}>
+        <VoteButton country={country} handleClick={toggleModal} />
+      </Grid>
+    </Grid>
+  </Card>
+);
+
+export default CountryCard;
