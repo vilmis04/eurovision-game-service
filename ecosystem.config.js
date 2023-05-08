b3e36c03c0397243910d3b90851146fb85113f88
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2023-05-08 21:48:34 +0300

    fix: add config

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+      {
+        name: 'ev-game-be',
+        script: './dist/apps/eurovision-game-backend/main.js',
+        instances: 1,
+        exec_mode: 'cluster',
+        env: {
+          NODE_ENV: 'production',
+        },
+      },
+    ],
+  };
