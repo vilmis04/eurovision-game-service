ed621afb62ccfc3661ee2fd224b1c4b65d7e5a9e
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2023-05-08 23:49:11 +0300

    update field width

--- apps/eurovision-game-ui/src/pages/GroupPage/GroupPage.config.tsx
+++ apps/eurovision-game-ui/src/pages/GroupPage/GroupPage.config.tsx
@@ -38,7 +38,7 @@ export const getPopupConfig = ({
 		cancelLabel: "Cancel",
 		component: (
 			<FormControlLabel
-				control={<FormField name="name" fullWidth />}
+				control={<FormField name="name" sx={{ width: "100%" }} />}
 				label={
 					<Typography
 						variant="body1"
